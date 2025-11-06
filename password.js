9732557f88ab2a3d58b6d2918ba8011619d12209
sarionflorence-create <sarionflorence@gmail.com>
2025-11-06 23:48:46 +0800

    Delete password.js

--- password.js
+++
@@ -1,8 +0,0 @@
-function check() {
-  const input = document.getElementById("pass").value;
-  if(input === "051626") {
-    window.location.href = "home.html"; // ← DITO TAMA NA
-  } else {
-    alert("Wrong password po baby 😅 Try again!");
-  }
-}
